921f1a5438be04e31b76d95c0b82db9f2537a7df
Jhorman Guetes M <jhormnjair21@gmail.com>
2022-03-09 22:03:22 -0500

    Login funcionando

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
+const userController = require('../controllers/user.controller');
+
 const router = express.Router();
 
 module.exports = () => {
-    router.get('/api/v1/prueba', (req, res)=>{
-        res.json({mazorca: "mazorca"});
-    })
+    router.get('/api/v1/auth', userController.auth);
+    router.post('/api/v1/register', userController.register);
+    router.get('/api/v1/list', userController.list);
+
 
     return router;
 }
